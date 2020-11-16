299cbaa5efec0548ca7c1f0424cb14330ed5bf0e
Matt <Matt@Mateis-MacBook-Pro.local>
2020-11-16 17:01:25 +0100

    losing my shit here lol

--- calculator.js
+++ calculator.js
@@ -1,3 +1,41 @@
+class Calculator {
+    constructor(prevOperand, currOperand) {
+        this.prevOperand = prevOperand;
+        this.currOperand = currOperand;
+        this.clear()
+
+    }
+
+clear (){
+    this.currOperand = "";
+    this.prevOperand = "";
+    this.operation = "undefined";
+
+}
+
+delete() {
+
+}
+
+appendNumber(number){
+
+}
+
+chooseOperation(operation){
+
+}
+
+compute(){
+
+}
+
+updateDisplay(){
+
+}
+
+}
+
+
 const body = document.querySelector("body");
 body.className = "body";
 
@@ -14,16 +52,18 @@ calcResult.className = "result";
 
 const prevOperand = document.createElement("div");
 prevOperand.className = "previous-operand";
+prevOperand.textContent = "123 +";
 
 // ____________________
 
 const currOperand = document.createElement('div');
 currOperand.className = "current-operand";
+currOperand.textContent = "432";
 
 // ____________________
 
 
-let tab = ["(", ")", "%", "AC", "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+"];
+let tab = ["(", ")", "%", "AC", "7", "8", "9", "/", "4", "5", "6", "x", "1", "2", "3", "-", "0", ".", "=", "+"];
 
 let line;
 
@@ -38,11 +78,21 @@ for (let i = 0; i < tab.length; i++){
     button.textContent = tab[i];
     button.className = "btn";
 
-if(i <= 3){
-    button.className = "btnOpp"
+if(i <= 2){
+    button.className = "btnOpp";
+}
+
+if((i == 7) || (i == 11) || (i == 15) || (i == 19)){
+    button.className = "operators";
+}
+if (i == 18){
+    button.className = "equal";
 }
 if(i % 4 === 3){
-    button.className = "btnOpp"
+    button.className = "btnOpp";
+}
+if (i == 3){
+    button.className = "allclear";
 }
 
 if(i%4===0){
@@ -54,3 +104,20 @@ if(i%4===0){
     line.appendChild(button);
 }
 
+// END OF CALCULATOR CREATION CODE 
+
+// START OF JS FUNCTIONALITY CODE
+
+const numberButtons = document.querySelectorAll(".btn");
+
+const oppButtons = document.querySelectorAll(".btnOpp");
+
+const equalButton = document.querySelector(".equal");
+
+const acButton = document.querySelector(".allclear");
+
+const calculator = new Calculator(prevOperand, currOperand);
+
+numberButtons.forEach(button => {
+    button.addEventListener
+})
